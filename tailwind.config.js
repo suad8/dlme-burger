b0c6f46e01651a178199bbb2ab1f8796f3321c34
suad8 <sauds771q@gmail.com>
2026-03-18 10:40:02 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'nmat-beige': '#F5F5DC', // درجة البيج المناسبة
+        'nmat-black': '#1a1a1a',  // أسود فخم
+      },
+      fontFamily: {
+        'nmat-font': ['Amiri', 'serif'], // خط كلاسيكي يشبه روح اللوجو
+      }
+    },
+  },
+}
